4f18dafa05d938867df0be3664a6f4fefacf93ba
CS-Tao <990296951@qq.com>
2017-10-23 10:44:28 +0800

    添加通过name查询景点的接口

--- View-Spot-of-City-WebService/routes/viewspot.js
+++ View-Spot-of-City-WebService/routes/viewspot.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var userInfo = require("./viewspot/getuserinfo");
+var viewinfo = require("./viewspot/getviewbyname");
+var viewcount = require("./viewspot/getviewcountbyname");
 
 router.get("/userinfo", userInfo.GetUserInfo);
+router.get("/viewinfo", viewinfo.GetViewByName);
+router.get("/viewcount", viewcount.GetViewCountByName);
 
 module.exports = router;
---
+++ View-Spot-of-City-WebService/routes/viewspot/getviewbyname.js
@@ -0,0 +1,34 @@
+"use strick"
+exports.__esModule = true;
+var mysql = require("mysql");
+
+function GetViewByName(req, res, next)
+{
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "admin",
+        password: "admin1997",
+        database: "ViewSpotOfWuhan"
+    });
+    var params = req.query;
+
+    var queryString = "SELECT * FROM ViewSpotData" + 
+    " WHERE name like '" + params.name + "';";
+    connection.query(queryString, function (err, results) {
+        if (err) {
+            res.json({
+                ViewInfo: []
+            });
+            return;
+        }
+        else {
+            res.json({
+                ViewInfo: results
+            });
+        }
+    });
+    connection.end();
+}
+
+exports.GetViewByName = GetViewByName;

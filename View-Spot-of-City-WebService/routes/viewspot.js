f97e8455667ac43109ce96200b7b50c378822904
CS-Tao <990296951@qq.com>
2017-10-23 11:21:24 +0800

    调试

--- View-Spot-of-City-WebService/routes/viewspot.js
+++ View-Spot-of-City-WebService/routes/viewspot.js
@@ -7,8 +7,8 @@ var viewcountbyneme = require("./viewspot/getviewcountbyname");
 var viewinfobylocate = require("./viewspot/getviewbylocate")
 
 router.get("/userinfo", userInfo.GetUserInfo);
-router.get("/viewinfobyname", viewinfo.GetViewByName);
-router.get("/viewcountbyname", viewcount.GetViewCountByName);
+router.get("/viewinfobyname", viewinfobyname.GetViewByName);
+router.get("/viewcountbyname", viewcountbyneme.GetViewCountByName);
 router.get("./viewinfobylocate", viewinfobylocate.GetViewInfoByLocate)
 
 module.exports = router;

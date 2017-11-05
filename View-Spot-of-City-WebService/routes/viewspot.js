0f03f837f67017dc3d9336524f0ba0f0387a7c56
周溢波 <13277032677@163.com>
2017-11-05 22:50:51 +0800

    按年获取人流量数据api

--- View-Spot-of-City-WebService/routes/viewspot.js
+++ View-Spot-of-City-WebService/routes/viewspot.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var userInfo = require("./viewspot/getuserinfo");
 var viewinfobyname = require("./viewspot/getviewbyname");
 var viewcountbyneme = require("./viewspot/getviewcountbyname");
-var viewinfobylocate = require("./viewspot/getviewbylocate")
+var viewinfobylocate = require("./viewspot/getviewbylocate");
+var visitorbyyear = require("./viewspot/getvisitorbyyear")
 
 router.get("/userinfo", userInfo.GetUserInfo);
 router.get("/viewinfobyname", viewinfobyname.GetViewByName);
 router.get("/viewcountbyname", viewcountbyneme.GetViewCountByName);
-router.get("/viewinfobylocate", viewinfobylocate.GetViewInfoByLocate)
+router.get("/viewinfobylocate", viewinfobylocate.GetViewInfoByLocate);
+router.get("/visitorbyyear", visitorbyyear.GetVisitorByYear)
 
 module.exports = router;
---
+++ View-Spot-of-City-WebService/routes/viewspot/getvisitorbyyear.js
@@ -0,0 +1,33 @@
+"use strick"
+exports.__esModule = true;
+var mysql = require("mysql");
+
+function GetVisitorByYear(req, res, next) {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "admin",
+        password: "admin1997",
+        database: "ViewSpotOfWuhan"
+    });
+    var params = req.query;
+        
+    var queryString = "SELECT * FROM Visitors" +
+        " WHERE Year = '" + params.year + "';";
+    connection.query(queryString, function (err, results) {
+        if (err) {
+            res.json({
+                VisitorInfo: err.message
+            });
+            return;
+        }
+        else {
+            res.json({
+                VisitorInfo: results
+            });
+        }
+    });
+    connection.end();
+}
+
+exports.GetVisitorByYear = GetVisitorByYear;

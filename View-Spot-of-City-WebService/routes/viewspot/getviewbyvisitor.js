90a5d1ecf61df7bed6f11b43ddbef62e568b981c
CS-Tao <990296951@qq.com>
2017-11-10 17:37:21 +0800

    debug

--- View-Spot-of-City-WebService/routes/viewspot/getviewbyvisitor.js
+++ View-Spot-of-City-WebService/routes/viewspot/getviewbyvisitor.js
@@ -17,8 +17,8 @@ function GetViewByVisitor(req, res, next) {
         "a.ViewId," +   
         "a.Visitors," +
         "a.Year," +
-        "lat as 'Lat'," +
-        "lng as 'Lng', name as 'ViewSpotName' From Visitors a INNER JOIN ViewSpotData b ON a.Id = b.id " +
+        "lat," +
+        "lng, name as 'ViewSpotName' From Visitors a INNER JOIN ViewSpotData b ON a.Id = b.id " +
         "where a.Month=" + params.month + " and a.Year=" + params.year + 
         " ORDER BY a.Visitors DESC LIMIT " + params.limit + ";";
     connection.query(queryString, function (err, results) {
